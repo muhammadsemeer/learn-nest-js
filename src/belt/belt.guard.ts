86ef892ea82370894732cdbe3e29d6b809868001
Muhammad Semeer <semeer@clusterdev.net>
2024-02-29 00:03:35 +0530

    Logging

--- src/belt/belt.guard.ts
+++ src/belt/belt.guard.ts
@@ -1,12 +1,30 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  Logger,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { Request } from 'express';
+import { IncomingHttpHeaders } from 'http';
+
+interface CustomHeaders extends IncomingHttpHeaders {
+  belt: string;
+}
 
 @Injectable()
 export class BeltGuard implements CanActivate {
+  private readonly logger = new Logger(BeltGuard.name);
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    const request = context.switchToHttp().getRequest();
+    const request = context
+      .switchToHttp()
+      .getRequest<Request & { headers: CustomHeaders }>();
+
+    this.logger.debug('Checking if the request has a black belt');
+    this.logger.log(request.headers);
+
     return request.headers.belt === 'black';
   }
 }
