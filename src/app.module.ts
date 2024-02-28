b82ef98d35addb28759c16595afef348e711a719
Muhammad Semeer <semeer@clusterdev.net>
2024-02-29 00:32:13 +0530

    Configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NinjasModule } from './ninjas/ninjas.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [NinjasModule],
+  imports: [ConfigModule.forRoot(), NinjasModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService);
+  const port = config.get('PORT') || 3000;
+  const nodeEnv = config.get('NODE_ENV') || 'development';
+  await app.listen(port);
+  Logger.log(
+    `Server running on http://localhost:${port} in ${nodeEnv} mode`,
+    'Bootstrap',
+  );
 }
 
 bootstrap();
